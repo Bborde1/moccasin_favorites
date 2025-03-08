c23ca1e37f790f5045cdaabfc07554384566cd51
Bborde1 <Brandon.Borde@gmail.com>
2025-03-08 00:30:34 -0500

    Added deploy script for favorites smart contract

---
+++ script/deploy.py
@@ -0,0 +1,11 @@
+from src import favorites
+
+
+def deploy():
+    favorites_contract = favorites.deploy()
+    starting_number = favorites_contract.retrieve()
+    print(f"Starting number is {starting_number}")
+
+
+def moccasin_main():
+    deploy()
